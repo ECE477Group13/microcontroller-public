861b289debacfcc4b19acb2d98df69bae008d03b
Reed <reed369@purdue.edu>
2023-09-14 21:21:26 -0400

    basic GPIO test - 38 input to 48 output

--- include/main.h
+++ include/main.h
@@ -1,2 +1,3 @@
+
 #include <stdio.h>
 #include "nvs_flash.h"
--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
 #include "main.h"
+#include "i2cmicro.h"
+
 void app_main() {
     // Init flash module
     nvs_flash_init();
+
+    // Init imu
+    // init_imu();
+    
+    gpio_set_direction(GPIO_NUM_48, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_NUM_38, GPIO_MODE_INPUT);
+    
+    while(true) {
+        gpio_set_level(GPIO_NUM_48, gpio_get_level(GPIO_NUM_38));
+
+    }
 
     // Init ble
     //init_ble();
