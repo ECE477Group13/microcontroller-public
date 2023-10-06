af42b8993bea64b3f15d4938459f936a991963c1
NickW09 <60638129+NickW09@users.noreply.github.com>
2023-10-06 17:48:19 -0400

    Initial TX code outline for I2S based on I2S IDF

--- include/i2smicro.h
+++ include/i2smicro.h
@@ -1,2 +1,10 @@
+//#include "driver/i2s.h"
+#include "driver/gpio.h"
 #include "driver/i2s_std.h"
-// #include "driver/i2s.h"
+//#include "driver/i2s_pdm.h" //Not needed?
+//#include "driver/i2s_tdm.h" //Not needed?
+
+#define I2S_DOUT GPIO_NUM_18 //Data out pin
+#define I2S_BCLK 0 //Bit clock
+#define I2S_LRC 0 //Left/right clock
+#define I2S_PORT 0 //Port number
